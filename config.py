6be3d36c3a870726ff7a970f488e1a1861d4d98f
emiliano <emipaz1975@hotmail.com>
2024-11-19 11:37:48 -0300

    codigos py del bot

---
+++ config.py
@@ -0,0 +1,102 @@
+MEMORIA = 10
+DOCUMENTOS = 5
+MODELO = "gpt-4o-mini"
+# MODELO = "gpt-3.5-turbo"
+# MODELO = "gpt-4-turbo"
+
+BASE_1  = "DE_C1_M1"
+CURSO_1 = "Introduccion al Data Engineering Modulo 1"
+ALIAS_1 = "DE-Nodulo-1"
+DESCRIPCION_1 = """Visión general de la ingeniería de datos y creación de canalizaciones de datos en la nube.
+Introducción a la Ingeniería de Datos - Módulo 1: Visión General del Curso
+
+"""
+TEMARIO_1 = """
+- Introducción a la ingeniería de datos.
+- Ciclo de vida de la ingeniería de datos.
+- Historia y contexto de la ingeniería de datos.
+- Valor agregado como ingeniero de datos.
+- Recolección de necesidades de los interesados.
+"""
+
+BASE_2  = "DE_C1_M2"
+CURSO_2 = "Introduccion al Data Engineering Modulo 2"
+ALIAS_2 = "DE-Modulo-2"
+DESCRIPCION_2 = """En esta semana, profundizaremos en el ciclo de vida 
+de la ingeniería de datos y sus aspectos subyacentes, 
+que se introdujeron en la semana anterior. 
+Aprenderemos sobre las etapas del ciclo de vida de los datos, 
+desde la generación hasta la disponibilidad para su uso en análisis y 
+aprendizaje automático.
+"""
+TEMARIO_2 = """
+- Profundización en cada etapa del ciclo de vida de la ingeniería de datos.
+- Teoría y orientación sobre la ingeniería de datos.
+- Práctica con pipelines de datos en la nube (AWS).
+"""
+
+BASE_3 = "DE_C1_M3"
+CURSO_3 = "Introduccion al Data Engineering Modulo 3"
+ALIAS_3 = "DE-Modulo-3"
+DESCRIPCION_3 = """Principios de una buena arquitectura de datos.
+En esta semana, profundizaremos en la construcción de una buena arquitectura de datos. 
+Aprenderemos cómo la arquitectura de datos se integra en el contexto más amplio de la 
+arquitectura empresarial y exploraremos ejemplos específicos de arquitectura. 
+También discutiremos principios orientadores para pensar como un arquitecto de datos."""
+TEMARIO_3 = """
+- Arquitectura de Datos y Arquitectura Empresarial
+- La arquitectura de datos es una parte fundamental de la arquitectura empresarial de una organización.
+- Pensar como un arquitecto es crucial para el éxito en el trabajo de un ingeniero de datos.
+- Ejemplos de Arquitectura
+  - Se presentarán ejemplos específicos de arquitecturas de datos.
+  - Se discutirá cómo traducir las necesidades de los interesados en decisiones tecnológicas para los sistemas de datos.
+- Principios Orientadores
+  - Se compartirán principios que ayudarán a desarrollar una mentalidad arquitectónica.
+- Laboratorio Práctico
+  - Evaluación de compensaciones en aspectos como:
+  - Costo
+  - Rendimiento
+  - Escalabilidad
+  - Seguridad
+- Análisis de una arquitectura de datos en la nube de AWS.
+ - Marco de Trabajo Bien Arquitectado de AWS
+- Exploración de un conjunto de principios que ayudan a diseñar sistemas de datos robustos y efectivos.
+"""
+
+BASE_4 = "DE_C1_M4"
+CURSO_4 = "Introduccion al Data Engineering Modulo 4"
+ALIAS_4 = "DE-Modulo-4"
+DESCRIPCION_4 = """En esta cuarta semana del curso, se integran los conceptos aprendidos 
+en las semanas anteriores sobre la recopilación de requisitos, el ciclo de vida de la 
+ingeniería de datos y la arquitectura de datos. 
+Se abordará un escenario práctico que incluye la definición de requisitos 
+funcionales y no funcionales, así como la selección de herramientas y 
+tecnologías para la implementación de sistemas en la nube."""
+TEMARIO_4 = """
+1. Recopilación de Requisitos
+- Conversaciones entre ingenieros de datos y científicos de datos.
+- Identificación de necesidades para proyectos de análisis y aprendizaje automático.
+- Importancia de involucrar a otros interesados, como el equipo de marketing y los ingenieros de software.
+2. Marco de Trabajo para Ingenieros de Datos
+- El proceso se puede dividir en los siguientes pasos:
+- Identificación de objetivos comerciales y necesidades de los interesados.
+- Definición de requisitos del sistema.
+- Selección de herramientas y tecnologías.
+- Construcción y despliegue del sistema.
+3. Detalles de Cada Paso
+- Paso 1: Preguntar sobre las acciones que tomarán los interesados.
+- Paso 2: Traducir las necesidades de los interesados en requisitos del sistema.
+- Paso 3: Prototipado y pruebas.
+- Paso 4: Evolución del sistema.
+4. Ciclo de Vida de la Ingeniería de Datos
+- En las semanas 2 y 3, se exploraron:
+- Componentes de los sistemas de datos.
+- Personas involucradas: Propietarios de sistemas de origen y usuarios finales.
+5. Principios de Buena Arquitectura de Datos
+- Planificación para fallos.
+- Selección de componentes comunes desde el inicio del diseño del sistema.
+6. Objetivos de la Semana
+- Integrar todos los conceptos aprendidos en un escenario práctico.
+- Traducir los requisitos en un diseño arquitectónico.
+- Construir el sistema en la nube de AWS.
+"""
